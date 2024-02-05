912db8b7455441112c98cb2ab83d713abd81176f
akash.gupta3203707 <akash.gupta@venturepact.com>
2024-02-05 21:44:20 +0530

    task 6 done

---
+++ DOM_MANUPLATION/Task6/index.js
@@ -0,0 +1,25 @@
+const basketHeading = document.getElementById("basket-heading");
+
+    if (basketHeading) {
+        basketHeading.style.color = "brown";
+    } else {
+        console.error("Element with id 'basket-heading' not found.");
+    }
+const evenFruitItems1 = document.querySelectorAll
+const evenFruitItems3 = document.querySelectorAll
+const evenFruitItems2 = document.querySelectorAll
+
+    // 2. Change the background color of even fruit items to red and text color to white.
+    const evenFruitItems = document.querySelectorAll(".fruits .fruit:nth-child(even)");
+    
+    evenFruitItems.forEach(fruit => {
+        fruit.style.backgroundColor = "brown";
+        fruit.style.color = "white";
+    });
+
+
+
+
+
+
+// Write answer to the questions asked below:
